1922729587b12dc886127f42915cf816ccc07f31
Koi-foo <eg.evgeniy@gmail.com>
2020-12-02 17:42:26 +0300

    Window title changed

--- form/main_win.py
+++ form/main_win.py
@@ -147,7 +147,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Управление модулями ядер"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Управление ядрами"))
         self.text_Question.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
